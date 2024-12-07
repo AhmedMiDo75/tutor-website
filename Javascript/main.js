626a174f62b901f474bc83fb1b04b10319b68a7d
AhmedMiDo75 <codetunefx@gmail.com>
2024-12-07 12:14:21 +0200

    Header-Finished

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,7 +1,18 @@
+// Toggle Mobile Menu
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".mobile-menu .mobile-menu-toggle");
   const mobileMenu = document.querySelector(".mobile-menu .mobile-menu-items");
   toggleBtn.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+});
+
+// Change navbar background on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (this.window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
 });
