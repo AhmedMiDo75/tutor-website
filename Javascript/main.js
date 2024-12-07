113a084e24b134ffbc6f5e3e2643fb8116bc6fab
AhmedMiDo75 <codetunefx@gmail.com>
2024-12-07 12:16:59 +0200

    alil fix

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // Change navbar background on scroll
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
-  if (this.window.scrollY > 0) {
+  if (window.scrollY > 0) {
     navbar.classList.add("navbar-scroll");
   } else {
     navbar.classList.remove("navbar-scroll");
